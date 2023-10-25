fb9004dadc3bf21700748cd4dbbd3ec90df5090a
akvine <redgooseman55@gmail.com>
2023-10-25 23:14:54 +0300

    git init

---
+++ src/main/java/ru/akvine/fitstats/FitStatsHubApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.fitstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitStatsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitStatsHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/fitstats/FitStatsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.fitstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitStatsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
