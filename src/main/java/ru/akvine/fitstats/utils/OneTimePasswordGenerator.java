2966669422326ba6c5c878c71515822ff92dbe31
akvine <redgooseman55@gmail.com>
2024-02-17 15:46:05 +0300

    remove useless TODO

--- src/main/java/ru/akvine/fitstats/utils/OneTimePasswordGenerator.java
+++ src/main/java/ru/akvine/fitstats/utils/OneTimePasswordGenerator.java
@@ -7,7 +7,6 @@ import java.security.SecureRandom;
 public class OneTimePasswordGenerator {
     private static final SecureRandom secureRandom = new SecureRandom();
 
-    // TODO : нужно разобраться, что это за маг. константы и почему именной такой алгоритм
     public static String generate(int length) {
         return Strings.padStart(secureRandom.nextInt((int) Math.pow(10.0D, length) -1) + "", length, '0');
     }
